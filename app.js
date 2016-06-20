aeb0a98954cafc48ca6b106837f68de36e8ae7d6
Arika <Delete0093@Gmail.com>
2016-06-20 21:11:10 +0900

    test

---
+++ app.js
@@ -0,0 +1,33 @@
+var http = require("http");
+var socketio = require("socket.io");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./index.html", "utf-8");
+     res.end(output);
+}).listen(process.env.PORT || 5000);
+
+var io = socketio.listen(server);
+io.configure(function(){
+     io.set('transports', ['xhr-polling']);
+     io.set('polling duration', 10);
+});
+
+io.sockets.on("connection", function (socket) {
+
+  // メッセージ送信（送信者にも送られる）
+  socket.on("C_to_S_message", function (data) {
+    io.sockets.emit("S_to_C_message", {value:data.value});
+  });
+
+  // ブロードキャスト（送信者以外の全員に送信）
+  socket.on("C_to_S_broadcast", function (data) {
+    socket.broadcast.emit("S_to_C_message", {value:data.value});
+  });
+
+  // 切断したときに送信
+  socket.on("disconnect", function () {
+//    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
+  });
+});
